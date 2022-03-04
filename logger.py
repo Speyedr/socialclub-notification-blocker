4444b0d13e66b57ae9fb98eb3ee89acd63f07072
Daniel "Speyedr" Summer <38199900+Speyedr@users.noreply.github.com>
2022-03-04 16:01:23 +1100

    Initial commit

---
+++ logger.py
@@ -0,0 +1,93 @@
+import queue
+from base64 import decodebytes
+from pydivert import Packet
+from datetime import datetime
+from enum import Enum, auto
+from multiprocessing import Process
+
+
+class BlockReason(Enum):
+    ALLOWED = 0
+    DROP_INC_80 = auto()
+    DROP_CLIENT_POST = auto()
+    DROP_LENGTH = auto()
+
+
+MAX_IP_LENGTH = len("255.255.255.255:12345")    # 21
+
+
+def str_pad_right(string, pad_to=MAX_IP_LENGTH, padding=" "):
+    pad_amount = pad_to - len(string)
+    if pad_amount < 0: raise ValueError("Couldn't pad string: length of string is greater than pad_to")
+    return string + (padding * pad_amount)
+
+
+class Logger:
+
+    def __init__(self, proxy_queue, where_to_save="logger.log"):
+        self.where_to_save = where_to_save
+        self.queue = proxy_queue
+        self.proc = Process(target=self.process_loop, args=())
+        self.proc.daemon = True
+        self.stop_processing = False
+
+    def add_packet(self, packet_as_base64, packet_interface, packet_direction,
+                   packet_allowed, block_reason=BlockReason.ALLOWED):
+        # This will be called from the filtering thread, pushing the encoded packet into the queue.
+        # The processing / logging is run in a different thread, which pops the packet and writes info to the file.
+        self.queue.put((packet_as_base64, packet_interface, packet_direction, packet_allowed, block_reason))
+
+    def process_item(self, blocking=True, timeout=3):
+        # If there is a packet then get it, else wait for one. Packets are encoded as base64 because the Packet class
+        # uses memoryview types which are great but you can't pickle them (i.e. can't pipe them to different processes).
+        try:
+            (packet_base64, interface, direction, allowed, reason) = self.queue.get(blocking, timeout)
+            packet = Packet(decodebytes(packet_base64), interface, direction)  # Decode the and recreate the packet.
+            self.add_message(Logger.construct_packet_debug_info(packet, allowed, reason))
+        except queue.Empty:
+            pass
+
+    def start(self):
+        self.proc.start()
+
+    def stop(self):
+        self.stop_processing = True     # We need this setting so that process_loop() will eventually return.
+        self.proc.terminate()
+
+    # This needs to be run in its' own thread, will infinitely process items as they're pushed into the queue.
+    # Note that we wait for packets, but timeout every 3 seconds so that we can keep checking the while loop condition.
+    def process_loop(self):
+        while not self.stop_processing:
+            self.process_item()
+
+    @staticmethod
+    def construct_packet_debug_info(packet, allowed, reason):
+        protocol = "UDP" if packet.udp is not None else "TCP"
+        decision = "ALLOWED" if allowed else "DROPPED"
+        rule = "" if reason == BlockReason.ALLOWED else reason.name
+        length = str(len(packet.payload))
+        source = packet.src_addr + ":" + str(packet.src_port)
+        destination = packet.dst_addr + ":" + str(packet.dst_port)
+        print("Parts:", protocol, destination, rule, length, source, destination)
+        message = protocol+" PACKET "+decision+" FROM "+str_pad_right(source)+" -> "+\
+            str_pad_right(destination)+" Len:"+str_pad_right(length, 8)+" Reason:"+rule
+        return message
+
+    def add_message(self, msg):
+        Logger.static_add_message(msg, self.where_to_save)
+
+    @staticmethod
+    def static_add_message(msg, where_to_save="logger.log"):
+        timestamp = "["+Logger.get_timestamp()+"] "
+        message = msg.replace('\n', '\n'+timestamp)   # If this message is multiline, add the timestamp to each line.
+        handle = Logger.get_handle(where_to_save)
+        handle.write(timestamp + message + '\n')
+        handle.close()
+
+    @staticmethod
+    def get_handle(file):
+        return open(file, "a")
+
+    @staticmethod
+    def get_timestamp():
+        return str(datetime.now().isoformat(sep=" "))
---
+++ setup.py
@@ -0,0 +1,77 @@
+"""
+Most of this build process was copied from DigitalArc's Guardian project.
+https://gitlab.com/digitalarc/guardian/-/blob/master/setup.py
+"""
+
+from cx_Freeze import setup, Executable
+import sys
+import zipfile
+import os
+import shutil
+
+zip_exclude_packages = ['pydivert']
+
+build_options = dict(packages=[], includes=['pydivert'],
+                     replace_paths=[("*", "")], optimize=2, zip_include_packages="*",
+                     zip_exclude_packages=zip_exclude_packages, silent=True)
+executables = [
+    Executable('main.py', targetName="SCBlocker.exe", icon="SCBlocker.ico",
+               copyright='Copyright (C) 2022 Daniel Summer')
+]
+
+version = "0.1.0"
+
+build_path = 'build/exe.win-amd64-{}.{}'.format(sys.version_info.major, sys.version_info.minor)
+
+if os.path.exists(build_path):
+    shutil.rmtree(build_path)
+
+if not os.path.exists('build/exe'):
+    os.makedirs('build/exe')
+
+if os.path.isfile('build/exe/guardian-{}.zip'.format(version)):
+    os.remove('build/exe/guardian-{}.zip'.format(version))
+
+
+setup(name='SocialClub Notification Blocker',
+      version=version,
+      description="Prevents SocialClub Notification Spam",
+      options=dict(build_exe=build_options),
+      executables=executables)
+
+
+def zip_folder(folder_path, output_path):
+    """Zip the contents of an entire folder (with that folder included
+    in the archive). Empty subfolders will be included in the archive
+    as well.
+
+    This function was copied from DigitalArc's Guardian project under the LGPLv3 license.
+    https://gitlab.com/digitalarc/guardian/-/blob/master/LICENSE
+    """
+    parent_folder = os.path.dirname(folder_path)
+    # Retrieve the paths of the folder contents.
+    contents = os.walk(folder_path, )
+    zip_file = zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED)
+
+    for root, folders, files in contents:
+        # Include all subfolders, including empty ones.
+        for folder_name in folders:
+            absolute_path = os.path.join(root, folder_name)
+            relative_path = absolute_path.replace(parent_folder + '\\',
+                                                  '')
+            zip_file.write(absolute_path, relative_path.replace(build_path, ''))
+        for file_name in files:
+            absolute_path = os.path.join(root, file_name)
+            relative_path = absolute_path.replace(parent_folder + '\\',
+                                                  '')
+            zip_file.write(absolute_path, relative_path.replace(build_path, ''))
+    zip_file.close()
+
+
+try:
+    shutil.copyfile('LICENSE', build_path + '/LICENSE')
+    shutil.copyfile('SOURCE', build_path + '/SOURCE')
+except:
+    pass
+
+zip_folder(build_path, 'build\exe\SCBlocker-{}.zip'.format(version))
