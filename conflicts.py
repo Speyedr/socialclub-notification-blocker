82387168f77d59e94bca37ffe84c88c2c3cbaf08
Daniel "Speyedr" Summer <38199900+Speyedr@users.noreply.github.com>
2022-03-08 21:07:40 +1100

    In order to iterate through currently running process, we'll be using an external library. Considering how many people are now using this project I don't think it's considerate to try and roll my own low-level version.

--- conflicts.py
+++ conflicts.py
@@ -3,3 +3,10 @@ Checks for specific programs or services that may conflict with SCBlocker.
 
 Author: Daniel "Speyedr" Summer
 """
+
+from psutil import process_iter
+
+# From https://www.reqrypt.org/windivert.html > Projects
+
+
+
