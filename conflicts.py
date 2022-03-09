f01e4885ecdb60fbd793a77fccbee8aa1c852de8
Daniel "Speyedr" Summer <38199900+Speyedr@users.noreply.github.com>
2022-03-09 19:48:13 +1100

    First attempt at combining matching process names as well as loaded modules inside those processes.

--- conflicts.py
+++ conflicts.py
@@ -4,34 +4,48 @@ Checks for specific programs or services that may conflict with SCBlocker.
 Author: Daniel "Speyedr" Summer
 """
 
-from psutil import process_iter
-from pygtrie import StringTrie
+from psutil import Process, process_iter
+from pygtrie import StringTrie, CharTrie
+from re import compile, match, search, MULTILINE
 
 # From https://www.reqrypt.org/windivert.html > Projects
-POSSIBLE_CONFLICTS = ["reqrypt"
-                      "suricata"
-                      "goodbyedpi"
-                      "barbatunnel"
-                      "streamdivert"
-                      "httpfilteringengine"
-                      "tallow"
-                      "clumsy"
-                      "inssidious"
-                      "lumogate.v1.0.1"
-                      "snoopspy3"
-                      "mitmproxy"
-                      "mitmdump"
-                      "mitmweb"
-                      "vpnhood"
-                      "warp"
-                      "warp-svc"]
+POSSIBLE_CONFLICTS = [
+                      "reqrypt",
+                      "suricata",
+                      "goodbyedpi",
+                      "barbatunnel",
+                      "streamdivert",
+                      "httpfilteringengine",
+                      "tallow",
+                      "clumsy",
+                      "inssidious",
+                      "lumogate",
+                      "snoopspy3",
+                      "mitmproxy",
+                      "mitmdump",
+                      "mitmweb",
+                      "vpnhood",
+                      "warp",
+                      "warp-svc"
+                     ]
+
+
+WINDIVERT_LIBRARIES = [
+                       "windivert",
+                       #"windivert32",  # Clever usage of CharTrie means we don't need to check 32 or 64
+                       #"windivert64"
+                      ]
 
 STRING_SEPARATOR = "\x00"      # process names can't have a null byte in them so will use as separator
 PROCESS_SUFFIX = ".exe"        # all process names end in .exe
+LIBRARY_SUFFIX = ".dll"        # all dynamic link libraries end in .dll
+
+# Matches the name of a file from a directory path
+FILE_GET_NAME = compile("(?<=\\\\)[^\\\\]+$")    # Backslash plague makes me want to cry
 
 
 def get_all_process_names():
-    return [process.name().lower() for process in process_iter()]
+    return [(process.name(), process.pid) for process in process_iter()]
 
 
 def construct_process_trie():
@@ -40,23 +54,56 @@ def construct_process_trie():
     structure. Naive comparisons of two texts against each other are O(m*n^2). Using a Trie structure, we can get this
     down to O(n+m), which scales *much* better when a lot of processes are running.
 
-    :return: a pygtrie.StringTrie containing all process names currently running on the computer.
+    :return: a pygtrie.CharTrie containing all process names currently running on the computer.
     """
 
-    trie = StringTrie(separator=STRING_SEPARATOR)
-    for process_name in get_all_process_names():
-        trie[process_name] = True   # The value stored could be the Process ID instead of True but I don't need it.
+    trie = CharTrie()
+    for (process_name, process_id) in get_all_process_names():
+        trie[process_name.lower()] = construct_module_trie(process_id)
+
     return trie
 
 
-def get_conflicts(trie, possible_conflicts=None):
-    if possible_conflicts is None:
-        possible_conflicts = POSSIBLE_CONFLICTS
+def construct_module_trie(process_id):
+    """
+    :param process_id:
+    :return: Trie containing all loaded modules in a process.
+    """
+    proc = Process(process_id)
+    trie = CharTrie()
+    for dll in proc.memory_maps():
+        filename = search(FILE_GET_NAME, dll.path)
+        if filename:
+            trie[filename.group().lower()] = dll.rss
+
+    return trie
+
+
+def get_conflicts(process_trie=None, process_conflicts=None):
+    if process_trie is None:
+        process_trie = construct_process_trie()
+
+    if process_conflicts is None:
+        process_conflicts = POSSIBLE_CONFLICTS
+
+    library_conflicts = WINDIVERT_LIBRARIES
 
     conflicts = []
-    for entry in possible_conflicts:
-        if trie.has_node(entry + PROCESS_SUFFIX) > 0:   # If there exists at least a node with this name from the head
-            conflicts.append(possible_conflicts)
+
+    # STEP 1: Any process names that match conflicting processes.
+    for conflict in process_conflicts:
+        matches = process_trie.keys(conflict)   # get any process names that match this conflict
+        while len(matches) > 0:                 # deplete list of matches
+            this_match = matches.pop()
+            conflicts.append(this_match)        # add the match to list of conflicts
+            del process_trie[this_match]        # remove it from the trie, we already know it's a conflict
+
+    # STEP 2: Any processes that contain conflicting modules.
+    for windivert in WINDIVERT_LIBRARIES:
+        for process_name, module_trie in process_trie.items():
+            matches = module_trie.keys(windivert)
+            if len(matches) > 0:                # there are modules in this process that match a conflict
+                conflicts.append(process_name)  # append the process. we don't actually care which modules matched
 
     return conflicts
 
