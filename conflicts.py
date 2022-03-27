04f2c0ba5a2c0ee2d23eeed2e08cba12a3607dc1
Daniel "Speyedr" Summer <38199900+Speyedr@users.noreply.github.com>
2022-03-27 17:19:38 +1100

    The logger could be heavily bottle-necking construct_module_trie(). Need to think of a way to use the async logger to lighten the load.

--- conflicts.py
+++ conflicts.py
@@ -3,11 +3,13 @@ Checks for specific programs or services that may conflict with SCBlocker.
 
 Author: Daniel "Speyedr" Summer
 """
+import multiprocessing
 
 from logger import Logger                           # :peepolove:
 from psutil import Process, process_iter, AccessDenied, NoSuchProcess
 from pygtrie import StringTrie, CharTrie
 from re import compile, match, search, MULTILINE
+from multiprocessing import Queue
 
 # From https://www.reqrypt.org/windivert.html > Projects
 POSSIBLE_CONFLICTS = [
@@ -46,6 +48,8 @@ PROCESS_SUFFIX = ".exe"        # all process names end in .exe
 LIBRARY_SUFFIX = ".dll"        # all dynamic link libraries end in .dll
 
 LOG_FILE = "conflicts.log"     # DEBUG: will be changed to debug.log in production
+#logger_queue = multiprocessing.Queue()
+#logger = Logger(logger_queue, LOG_FILE)
 
 # Matches the name of a file from a directory path
 FILE_GET_NAME = compile("(?<=\\\\)[^\\\\]+$")    # Backslash plague makes me want to cry
@@ -84,7 +88,7 @@ def construct_module_trie(process_id):
         proc = Process(process_id)
     except (AccessDenied, NoSuchProcess) as e:
         Logger.static_add_message("WARNING: Could not open process ID " + str(process_id) +
-                                  "\nReason: " + str(e))
+                                  "\nReason: " + str(e), LOG_FILE)
         return trie
     # Otherwise, we were able to open the process.
 
@@ -95,7 +99,7 @@ def construct_module_trie(process_id):
                 trie[filename.group().lower()] = dll.rss
     except (AccessDenied, NoSuchProcess) as e:     # Could not open process
         Logger.static_add_message("WARNING: Could not get modules for process ID " + str(process_id) +
-                                  "\nReason: " + str(e))
+                                  "\nReason: " + str(e), LOG_FILE)
 
     return trie
 
