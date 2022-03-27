b2236d2690ea181febc816d45fbafc211b2ab203
Daniel "Speyedr" Summer <38199900+Speyedr@users.noreply.github.com>
2022-03-27 16:41:26 +1100

    camelCase should not be used in build arguments as they will be deprecated at some point

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ build_options = dict(packages=[], includes=['pydivert'],
                      replace_paths=[("*", "")], optimize=2, zip_include_packages="*",
                      zip_exclude_packages=zip_exclude_packages, silent=True)
 executables = [
-    Executable('main.py', targetName="SCBlocker.exe", icon="icon.ico",
+    Executable('main.py', target_name="SCBlocker.exe", icon="icon.ico",
                copyright='Copyright (C) 2022 Daniel Summer', uac_admin=True)
 ]
 
